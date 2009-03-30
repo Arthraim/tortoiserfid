3e187734c2049c955571ebca010dd9d246ef7bd6
xuyang22 <xuyang22@cf1a9d58-13b4-11de-b69a-45dc1c5e4070>
2009-03-30 05:30:25 +0000

    2009-03-30. Add input pad on UI. Input function is complete.

--- mainwindow.h
+++ mainwindow.h
@@ -21,8 +21,23 @@ public:
 private:
     Ui::MainWindowClass *ui;
     RFIDfunction _RFIDfunction;
+    uint _inputNum;
+
+    void ShowNumber();
 
 private slots:
+    void on_btn_C_clicked();
+    void on_btn_CE_clicked();
+    void on_btn_0_clicked();
+    void on_btn_9_clicked();
+    void on_btn_8_clicked();
+    void on_btn_7_clicked();
+    void on_btn_6_clicked();
+    void on_btn_5_clicked();
+    void on_btn_4_clicked();
+    void on_btn_3_clicked();
+    void on_btn_2_clicked();
+    void on_btn_1_clicked();
     void on_btn_GetType_clicked();
     void on_btn_CutMoney_clicked();
     void on_btn_AddMoney_clicked();
